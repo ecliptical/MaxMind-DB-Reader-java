2f2f781519a62c9f7f7f285f66c8d1c99fb2a4ad
pnehrer <pnehrer@eclipticalsoftware.com>
2015-03-11 01:28:21 -0400

    Removed unnecessary byte array duplication when stream length is known up-front.

--- src/main/java/com/maxmind/db/BufferHolder.java
+++ src/main/java/com/maxmind/db/BufferHolder.java
@@ -1,6 +1,7 @@
 package com.maxmind.db;
 
 import java.io.ByteArrayOutputStream;
+import java.io.EOFException;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,70 +13,93 @@ import java.nio.channels.FileChannel.MapMode;
 import com.maxmind.db.Reader.FileMode;
 
 final class BufferHolder {
-    // DO NOT PASS OUTSIDE THIS CLASS. Doing so will remove thread safety.
-    private final ByteBuffer buffer;
+	// DO NOT PASS OUTSIDE THIS CLASS. Doing so will remove thread safety.
+	private final ByteBuffer buffer;
 
-    BufferHolder(File database, FileMode mode) throws IOException {
-        final RandomAccessFile file = new RandomAccessFile(database, "r");
-        boolean threw = true;
-        try {
-            final FileChannel channel = file.getChannel();
-            if (mode == FileMode.MEMORY) {
-                this.buffer = ByteBuffer.wrap(new byte[(int) channel.size()]);
-                if (channel.read(this.buffer) != this.buffer.capacity()) {
-                    throw new IOException("Unable to read "
-                            + database.getName()
-                            + " into memory. Unexpected end of stream.");
-                }
-            } else {
-                this.buffer = channel.map(MapMode.READ_ONLY, 0, channel.size());
-            }
-            threw = false;
-        } finally {
-            try {
-                // Also closes the underlying channel.
-                file.close();
-            } catch (final IOException e) {
-                // If an exception was underway when we entered the finally
-                // block, don't stomp over it due to an error closing the file
-                // and channel.
-                if (!threw) {
-                    throw e;
-                }
-            }
-        }
-    }
+	BufferHolder(File database, FileMode mode) throws IOException {
+		final RandomAccessFile file = new RandomAccessFile(database, "r");
+		boolean threw = true;
+		try {
+			final FileChannel channel = file.getChannel();
+			if (mode == FileMode.MEMORY) {
+				this.buffer = ByteBuffer.wrap(new byte[(int) channel.size()]);
+				if (channel.read(this.buffer) != this.buffer.capacity()) {
+					throw new IOException("Unable to read "
+							+ database.getName()
+							+ " into memory. Unexpected end of stream.");
+				}
+			} else {
+				this.buffer = channel.map(MapMode.READ_ONLY, 0, channel.size());
+			}
+			threw = false;
+		} finally {
+			try {
+				// Also closes the underlying channel.
+				file.close();
+			} catch (final IOException e) {
+				// If an exception was underway when we entered the finally
+				// block, don't stomp over it due to an error closing the file
+				// and channel.
+				if (!threw) {
+					throw e;
+				}
+			}
+		}
+	}
 
-    /**
-     * Construct a ThreadBuffer from the provided URL.
-     *
-     * @param stream the source of my bytes.
-     * @throws IOException          if unable to read from your source.
-     * @throws NullPointerException if you provide a NULL InputStream
-     */
-    BufferHolder(InputStream stream) throws IOException {
-        if (null == stream) {
-            throw new NullPointerException("Unable to use a NULL InputStream");
-        }
-        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        final byte[] bytes = new byte[16 * 1024];
-        int br;
-        while (-1 != (br = stream.read(bytes))) {
-            baos.write(bytes, 0, br);
-        }
-        this.buffer = ByteBuffer.wrap(baos.toByteArray());
-    }
+	/**
+	 * Construct a ThreadBuffer from the provided URL.
+	 *
+	 * @param stream the source of my bytes.
+	 * @throws IOException          if unable to read from your source.
+	 * @throws NullPointerException if you provide a NULL InputStream
+	 */
+	BufferHolder(InputStream stream) throws IOException {
+		if (null == stream) {
+			throw new NullPointerException("Unable to use a NULL InputStream");
+		}
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		final byte[] bytes = new byte[16 * 1024];
+		int br;
+		while (-1 != (br = stream.read(bytes))) {
+			baos.write(bytes, 0, br);
+		}
+		this.buffer = ByteBuffer.wrap(baos.toByteArray());
+	}
 
-    // This is just to ease unit testing
-    BufferHolder(ByteBuffer buffer) {
-        this.buffer = buffer;
-    }
+	/**
+	 * Construct a ThreadBuffer from the provided InputStream.
+	 *
+	 * @param stream the source of my bytes.
+	 * @param length number of bytes to read
+	 * @throws IOException          if unable to read from your source.
+	 * @throws NullPointerException if you provide a NULL InputStream
+	 */
+	BufferHolder(InputStream stream, int length) throws IOException {
+		if (null == stream) {
+			throw new NullPointerException("Unable to use a NULL InputStream");
+		}
+		this.buffer = ByteBuffer.allocate(length);
+		while (this.buffer.hasRemaining()) {
+			int c = stream.read(this.buffer.array(), this.buffer.arrayOffset() + this.buffer.position(), this.buffer.remaining());
+			if (c == -1)
+				throw new EOFException();
 
-    /*
-     * Returns a duplicate of the underlying ByteBuffer. The returned ByteBuffer
-     * should not be shared between threads.
-     */
-    synchronized ByteBuffer get() {
-        return this.buffer.duplicate();
-    }
+			this.buffer.position(this.buffer.position() + c);
+		}
+		this.buffer.flip();
+	}
+
+	// This is just to ease unit testing
+	BufferHolder(ByteBuffer buffer) {
+		this.buffer = buffer;
+	}
+
+	/*
+	 * Returns a duplicate of the underlying ByteBuffer. The returned ByteBuffer
+	 * should not be shared between threads.
+	 */
+	synchronized ByteBuffer get() {
+		return this.buffer.duplicate();
+	}
 }
--- src/main/java/com/maxmind/db/Reader.java
+++ src/main/java/com/maxmind/db/Reader.java
@@ -15,248 +15,260 @@ import com.fasterxml.jackson.databind.JsonNode;
  * addresses can be looked up using the <code>get</code> method.
  */
 public final class Reader implements Closeable {
-    private static final int DATA_SECTION_SEPARATOR_SIZE = 16;
-    private static final byte[] METADATA_START_MARKER = {(byte) 0xAB,
-            (byte) 0xCD, (byte) 0xEF, 'M', 'a', 'x', 'M', 'i', 'n', 'd', '.',
-            'c', 'o', 'm'};
+	private static final int DATA_SECTION_SEPARATOR_SIZE = 16;
+	private static final byte[] METADATA_START_MARKER = {(byte) 0xAB,
+		(byte) 0xCD, (byte) 0xEF, 'M', 'a', 'x', 'M', 'i', 'n', 'd', '.',
+		'c', 'o', 'm'};
 
-    private final int ipV4Start;
-    private final Metadata metadata;
-    private final AtomicReference<BufferHolder> bufferHolderReference;
+	private final int ipV4Start;
+	private final Metadata metadata;
+	private final AtomicReference<BufferHolder> bufferHolderReference;
 
-    /**
-     * The file mode to use when opening a MaxMind DB.
-     */
-    public enum FileMode {
-        /**
-         * The default file mode. This maps the database to virtual memory. This
-         * often provides similar performance to loading the database into real
-         * memory without the overhead.
-         */
-        MEMORY_MAPPED,
-        /**
-         * Loads the database into memory when the reader is constructed.
-         */
-        MEMORY
-    }
+	/**
+	 * The file mode to use when opening a MaxMind DB.
+	 */
+	public enum FileMode {
+		/**
+		 * The default file mode. This maps the database to virtual memory. This
+		 * often provides similar performance to loading the database into real
+		 * memory without the overhead.
+		 */
+		MEMORY_MAPPED,
+		/**
+		 * Loads the database into memory when the reader is constructed.
+		 */
+		MEMORY
+	}
 
-    /**
-     * Constructs a Reader for the MaxMind DB format. The file passed to it must
-     * be a valid MaxMind DB file such as a GeoIP2 database file.
-     *
-     * @param database the MaxMind DB file to use.
-     * @throws IOException if there is an error opening or reading from the file.
-     */
-    public Reader(File database) throws IOException {
-        this(database, FileMode.MEMORY_MAPPED);
-    }
+	/**
+	 * Constructs a Reader for the MaxMind DB format. The file passed to it must
+	 * be a valid MaxMind DB file such as a GeoIP2 database file.
+	 *
+	 * @param database the MaxMind DB file to use.
+	 * @throws IOException if there is an error opening or reading from the file.
+	 */
+	public Reader(File database) throws IOException {
+		this(database, FileMode.MEMORY_MAPPED);
+	}
 
-    /**
-     * Constructs a Reader as if in mode {@link FileMode#MEMORY}, without using
-     * a <code>File</code> instance.
-     *
-     * @param source the InputStream that contains the MaxMind DB file.
-     * @throws IOException if there is an error reading from the Stream.
-     */
-    public Reader(InputStream source) throws IOException {
-        this(new BufferHolder(source), "<InputStream>");
-    }
+	/**
+	 * Constructs a Reader as if in mode {@link FileMode#MEMORY}, without using
+	 * a <code>File</code> instance.
+	 *
+	 * @param source the InputStream that contains the MaxMind DB file.
+	 * @throws IOException if there is an error reading from the Stream.
+	 */
+	public Reader(InputStream source) throws IOException {
+		this(new BufferHolder(source), "<InputStream>");
+	}
 
-    /**
-     * Constructs a Reader for the MaxMind DB format. The file passed to it must
-     * be a valid MaxMind DB file such as a GeoIP2 database file.
-     *
-     * @param database the MaxMind DB file to use.
-     * @param fileMode the mode to open the file with.
-     * @throws IOException if there is an error opening or reading from the file.
-     */
-    public Reader(File database, FileMode fileMode) throws IOException {
-        this(new BufferHolder(database, fileMode), database.getName());
-    }
+	/**
+	 * Constructs a Reader as if in mode {@link FileMode#MEMORY}, without using
+	 * a <code>File</code> instance.
+	 *
+	 * @param source the InputStream that contains the MaxMind DB file.
+	 * @param source the length (in bytes) of the MaxMind DB file.
+	 * @throws IOException if there is an error reading from the Stream.
+	 */
+	public Reader(InputStream source, int length) throws IOException {
+		this(new BufferHolder(source, length), "<InputStream>");
+	}
 
-    private Reader(BufferHolder bufferHolder, String name) throws IOException {
-        this.bufferHolderReference = new AtomicReference<BufferHolder>(
-                bufferHolder);
+	/**
+	 * Constructs a Reader for the MaxMind DB format. The file passed to it must
+	 * be a valid MaxMind DB file such as a GeoIP2 database file.
+	 *
+	 * @param database the MaxMind DB file to use.
+	 * @param fileMode the mode to open the file with.
+	 * @throws IOException if there is an error opening or reading from the file.
+	 */
+	public Reader(File database, FileMode fileMode) throws IOException {
+		this(new BufferHolder(database, fileMode), database.getName());
+	}
 
-        ByteBuffer buffer = bufferHolder.get();
-        int start = this.findMetadataStart(buffer, name);
+	private Reader(BufferHolder bufferHolder, String name) throws IOException {
+		this.bufferHolderReference = new AtomicReference<BufferHolder>(
+				bufferHolder);
 
-        Decoder metadataDecoder = new Decoder(buffer, start);
-        this.metadata = new Metadata(metadataDecoder.decode(start).getNode());
+		ByteBuffer buffer = bufferHolder.get();
+		int start = this.findMetadataStart(buffer, name);
 
-        this.ipV4Start = this.findIpV4StartNode(buffer);
-    }
+		Decoder metadataDecoder = new Decoder(buffer, start);
+		this.metadata = new Metadata(metadataDecoder.decode(start).getNode());
 
-    /**
-     * Looks up the <code>address</code> in the MaxMind DB.
-     *
-     * @param ipAddress the IP address to look up.
-     * @return the record for the IP address.
-     * @throws IOException if a file I/O error occurs.
-     */
-    public JsonNode get(InetAddress ipAddress) throws IOException {
-        ByteBuffer buffer = this.getBufferHolder().get();
-        int pointer = this.findAddressInTree(buffer, ipAddress);
-        if (pointer == 0) {
-            return null;
-        }
-        return this.resolveDataPointer(buffer, pointer);
-    }
+		this.ipV4Start = this.findIpV4StartNode(buffer);
+	}
 
-    private BufferHolder getBufferHolder() throws ClosedDatabaseException {
-        BufferHolder bufferHolder = this.bufferHolderReference.get();
-        if (bufferHolder == null) {
-            throw new ClosedDatabaseException();
-        }
-        return bufferHolder;
-    }
+	/**
+	 * Looks up the <code>address</code> in the MaxMind DB.
+	 *
+	 * @param ipAddress the IP address to look up.
+	 * @return the record for the IP address.
+	 * @throws IOException if a file I/O error occurs.
+	 */
+	public JsonNode get(InetAddress ipAddress) throws IOException {
+		ByteBuffer buffer = this.getBufferHolder().get();
+		int pointer = this.findAddressInTree(buffer, ipAddress);
+		if (pointer == 0) {
+			return null;
+		}
+		return this.resolveDataPointer(buffer, pointer);
+	}
 
-    private int findAddressInTree(ByteBuffer buffer, InetAddress address)
-            throws InvalidDatabaseException {
-        byte[] rawAddress = address.getAddress();
+	private BufferHolder getBufferHolder() throws ClosedDatabaseException {
+		BufferHolder bufferHolder = this.bufferHolderReference.get();
+		if (bufferHolder == null) {
+			throw new ClosedDatabaseException();
+		}
+		return bufferHolder;
+	}
 
-        int bitLength = rawAddress.length * 8;
-        int record = this.startNode(bitLength);
+	private int findAddressInTree(ByteBuffer buffer, InetAddress address)
+			throws InvalidDatabaseException {
+		byte[] rawAddress = address.getAddress();
 
-        for (int i = 0; i < bitLength; i++) {
-            if (record >= this.metadata.getNodeCount()) {
-                break;
-            }
-            int b = 0xFF & rawAddress[i / 8];
-            int bit = 1 & (b >> 7 - (i % 8));
-            record = this.readNode(buffer, record, bit);
-        }
-        if (record == this.metadata.getNodeCount()) {
-            // record is empty
-            return 0;
-        } else if (record > this.metadata.getNodeCount()) {
-            // record is a data pointer
-            return record;
-        }
-        throw new InvalidDatabaseException("Something bad happened");
-    }
+		int bitLength = rawAddress.length * 8;
+		int record = this.startNode(bitLength);
 
-    private int startNode(int bitLength) {
-        // Check if we are looking up an IPv4 address in an IPv6 tree. If this
-        // is the case, we can skip over the first 96 nodes.
-        if (this.metadata.getIpVersion() == 6 && bitLength == 32) {
-            return this.ipV4Start;
-        }
-        // The first node of the tree is always node 0, at the beginning of the
-        // value
-        return 0;
-    }
+		for (int i = 0; i < bitLength; i++) {
+			if (record >= this.metadata.getNodeCount()) {
+				break;
+			}
+			int b = 0xFF & rawAddress[i / 8];
+			int bit = 1 & (b >> 7 - (i % 8));
+			record = this.readNode(buffer, record, bit);
+		}
+		if (record == this.metadata.getNodeCount()) {
+			// record is empty
+			return 0;
+		} else if (record > this.metadata.getNodeCount()) {
+			// record is a data pointer
+			return record;
+		}
+		throw new InvalidDatabaseException("Something bad happened");
+	}
 
-    private int findIpV4StartNode(ByteBuffer buffer)
-            throws InvalidDatabaseException {
-        if (this.metadata.getIpVersion() == 4) {
-            return 0;
-        }
+	private int startNode(int bitLength) {
+		// Check if we are looking up an IPv4 address in an IPv6 tree. If this
+		// is the case, we can skip over the first 96 nodes.
+		if (this.metadata.getIpVersion() == 6 && bitLength == 32) {
+			return this.ipV4Start;
+		}
+		// The first node of the tree is always node 0, at the beginning of the
+		// value
+		return 0;
+	}
 
-        int node = 0;
-        for (int i = 0; i < 96 && node < this.metadata.getNodeCount(); i++) {
-            node = this.readNode(buffer, node, 0);
-        }
-        return node;
-    }
+	private int findIpV4StartNode(ByteBuffer buffer)
+			throws InvalidDatabaseException {
+		if (this.metadata.getIpVersion() == 4) {
+			return 0;
+		}
 
-    private int readNode(ByteBuffer buffer, int nodeNumber, int index)
-            throws InvalidDatabaseException {
-        int baseOffset = nodeNumber * this.metadata.getNodeByteSize();
+		int node = 0;
+		for (int i = 0; i < 96 && node < this.metadata.getNodeCount(); i++) {
+			node = this.readNode(buffer, node, 0);
+		}
+		return node;
+	}
 
-        switch (this.metadata.getRecordSize()) {
-            case 24:
-                buffer.position(baseOffset + index * 3);
-                return Decoder.decodeInteger(buffer, 0, 3);
-            case 28:
-                int middle = buffer.get(baseOffset + 3);
+	private int readNode(ByteBuffer buffer, int nodeNumber, int index)
+			throws InvalidDatabaseException {
+		int baseOffset = nodeNumber * this.metadata.getNodeByteSize();
 
-                if (index == 0) {
-                    middle = (0xF0 & middle) >>> 4;
-                } else {
-                    middle = 0x0F & middle;
-                }
-                buffer.position(baseOffset + index * 4);
-                return Decoder.decodeInteger(buffer, middle, 3);
-            case 32:
-                buffer.position(baseOffset + index * 4);
-                return Decoder.decodeInteger(buffer, 0, 4);
-            default:
-                throw new InvalidDatabaseException("Unknown record size: "
-                        + this.metadata.getRecordSize());
-        }
-    }
+		switch (this.metadata.getRecordSize()) {
+		case 24:
+			buffer.position(baseOffset + index * 3);
+			return Decoder.decodeInteger(buffer, 0, 3);
+		case 28:
+			int middle = buffer.get(baseOffset + 3);
 
-    private JsonNode resolveDataPointer(ByteBuffer buffer, int pointer)
-            throws IOException {
-        int resolved = (pointer - this.metadata.getNodeCount())
-                + this.metadata.getSearchTreeSize();
+			if (index == 0) {
+				middle = (0xF0 & middle) >>> 4;
+			} else {
+				middle = 0x0F & middle;
+			}
+			buffer.position(baseOffset + index * 4);
+			return Decoder.decodeInteger(buffer, middle, 3);
+		case 32:
+			buffer.position(baseOffset + index * 4);
+			return Decoder.decodeInteger(buffer, 0, 4);
+		default:
+			throw new InvalidDatabaseException("Unknown record size: "
+					+ this.metadata.getRecordSize());
+		}
+	}
 
-        if (resolved >= buffer.capacity()) {
-            throw new InvalidDatabaseException(
-                    "The MaxMind DB file's search tree is corrupt: "
-                            + "contains pointer larger than the database.");
-        }
+	private JsonNode resolveDataPointer(ByteBuffer buffer, int pointer)
+			throws IOException {
+		int resolved = (pointer - this.metadata.getNodeCount())
+				+ this.metadata.getSearchTreeSize();
 
-        // We only want the data from the decoder, not the offset where it was
-        // found.
-        Decoder decoder = new Decoder(buffer, this.metadata.getSearchTreeSize()
-                + DATA_SECTION_SEPARATOR_SIZE);
-        return decoder.decode(resolved).getNode();
-    }
+		if (resolved >= buffer.capacity()) {
+			throw new InvalidDatabaseException(
+					"The MaxMind DB file's search tree is corrupt: "
+							+ "contains pointer larger than the database.");
+		}
 
-    /*
-     * Apparently searching a file for a sequence is not a solved problem in
-     * Java. This searches from the end of the file for metadata start.
-     *
-     * This is an extremely naive but reasonably readable implementation. There
-     * are much faster algorithms (e.g., Boyer-Moore) for this if speed is ever
-     * an issue, but I suspect it won't be.
-     */
-    private int findMetadataStart(ByteBuffer buffer, String databaseName)
-            throws InvalidDatabaseException {
-        int fileSize = buffer.capacity();
+		// We only want the data from the decoder, not the offset where it was
+		// found.
+		Decoder decoder = new Decoder(buffer, this.metadata.getSearchTreeSize()
+				+ DATA_SECTION_SEPARATOR_SIZE);
+		return decoder.decode(resolved).getNode();
+	}
 
-        FILE:
-        for (int i = 0; i < fileSize - METADATA_START_MARKER.length + 1; i++) {
-            for (int j = 0; j < METADATA_START_MARKER.length; j++) {
-                byte b = buffer.get(fileSize - i - j - 1);
-                if (b != METADATA_START_MARKER[METADATA_START_MARKER.length - j
-                        - 1]) {
-                    continue FILE;
-                }
-            }
-            return fileSize - i;
-        }
-        throw new InvalidDatabaseException(
-                "Could not find a MaxMind DB metadata marker in this file ("
-                        + databaseName + "). Is this a valid MaxMind DB file?");
-    }
+	/*
+	 * Apparently searching a file for a sequence is not a solved problem in
+	 * Java. This searches from the end of the file for metadata start.
+	 *
+	 * This is an extremely naive but reasonably readable implementation. There
+	 * are much faster algorithms (e.g., Boyer-Moore) for this if speed is ever
+	 * an issue, but I suspect it won't be.
+	 */
+	private int findMetadataStart(ByteBuffer buffer, String databaseName)
+			throws InvalidDatabaseException {
+		int fileSize = buffer.capacity();
 
-    /**
-     * @return the metadata for the MaxMind DB file.
-     */
-    public Metadata getMetadata() {
-        return this.metadata;
-    }
+		FILE:
+			for (int i = 0; i < fileSize - METADATA_START_MARKER.length + 1; i++) {
+				for (int j = 0; j < METADATA_START_MARKER.length; j++) {
+					byte b = buffer.get(fileSize - i - j - 1);
+					if (b != METADATA_START_MARKER[METADATA_START_MARKER.length - j
+					                               - 1]) {
+						continue FILE;
+					}
+				}
+				return fileSize - i;
+			}
+		throw new InvalidDatabaseException(
+				"Could not find a MaxMind DB metadata marker in this file ("
+						+ databaseName + "). Is this a valid MaxMind DB file?");
+	}
 
-    /**
+	/**
+	 * @return the metadata for the MaxMind DB file.
+	 */
+	public Metadata getMetadata() {
+		return this.metadata;
+	}
+
+	/**
      /**
-     * <p>
-     * Closes the database.
-     * </p>
-     * <p>
-     * If you are using <code>FileMode.MEMORY_MAPPED</code>, this will
-     * <em>not</em> unmap the underlying file due to a limitation in Java's
-     * <code>MappedByteBuffer</code>. It will however set the reference to
-     * the buffer to <code>null</code>, allowing the garbage collector to
-     * collect it.
-     * </p>
-     *
-     * @throws IOException if an I/O error occurs.
-     */
-    @Override
-    public void close() throws IOException {
-        this.bufferHolderReference.set(null);
-    }
+	 * <p>
+	 * Closes the database.
+	 * </p>
+	 * <p>
+	 * If you are using <code>FileMode.MEMORY_MAPPED</code>, this will
+	 * <em>not</em> unmap the underlying file due to a limitation in Java's
+	 * <code>MappedByteBuffer</code>. It will however set the reference to
+	 * the buffer to <code>null</code>, allowing the garbage collector to
+	 * collect it.
+	 * </p>
+	 *
+	 * @throws IOException if an I/O error occurs.
+	 */
+	@Override
+	public void close() throws IOException {
+		this.bufferHolderReference.set(null);
+	}
 }
